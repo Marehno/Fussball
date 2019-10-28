c994b71f98f25ce366cb78d5f1c80c3e962b1bd6
Marehno <blablacar325@web.de>
2019-10-28 18:22:08 +0100

    Projekt fertig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/Fussball.ts
@@ -0,0 +1,5 @@
+export class Fussball {
+  constructor(public id: number, public name: string, public position: string, public alter: number) {
+
+  }
+}
